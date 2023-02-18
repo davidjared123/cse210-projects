82da13daa156c4f67d7318e708cf6065da8610a3
David Perez <davidjared123@gmail.com>
2023-02-18 15:07:04 -0400

    Activities fundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running("April 24, 2023 Runing", 30, 3);
+        Bicycles bicycles = new Bicycles(40, "December 21, 2022 Bicycles", 20);
+        Swimming swimming = new Swimming("February 20, 2023 Swimming", 15, 30);
+
+        activities.Add(running);
+        activities.Add(bicycles);
+        activities.Add(swimming);
+
+        foreach (Activity item in activities)
+        {
+            Console.WriteLine(item.GetSumary());
+        }
     }
 }
